7821c1d42e2ebd9d234dea3495854acb0361689d
Johanna Gomez <pao_gomez1@hotmail.com>
2017-11-17 10:25:43 -0600

    console exercises included

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    /* making a new object */
+    public static void main(String[] args) {
+//        String userInput;
+        Scanner scan = new Scanner(System.in);
+//        System.out.println("Hello, please input your name");
+//        userInput = scan.nextLine();
+//        userInput = scan.next();
+//        System.out.println("hi " + userInput + "! It's nice to meet you.  My name is Pepe");
+
+
+        // ============================ Explore the Scanner Class=======================================================
+
+        //1.-   Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
+        System.out.println("what is your favorite number");
+        int userNumber = scan.nextInt();
+        System.out.println("you told me your fav number is : " +  userNumber);
+        scan.nextLine();   // Consume newline left-over and let me include the 3rd word
+
+        //2.-   Prompt a user to enter 3 words and store each of them in a separate variable, then display them back, each on a newline.
+        String firstWord, secondWord, thirdWord;
+        System.out.println("Hello, please enter 3 words:");
+        firstWord = scan.next();
+        secondWord = scan.next();
+        thirdWord = scan.next();
+        System.out.println("your 3 words were: " + " " + firstWord + " "  + secondWord + " " + thirdWord);
+        scan.nextLine();   // Consume newline left-over and let me include the sentence
+
+        //3.- and 4.- Prompt a user to enter a sentence, then store that sentence in a String variable using
+        // the .next method, then display that sentence back to the user.
+        String inputASentence;
+        System.out.println("please enter a sentence: ");
+        inputASentence = scan.nextLine();
+        System.out.println("this is your whole sentence: " + inputASentence);
+
+
+
+        // ============================ Calculate the perimeter and area of Codeup's classrooms========================
+
+        // 1.-  Prompt the user to enter values of length and width of the classroom.
+        System.out.println("what is the length, width and hight (in that order) of your classroom??:");
+        float length, width, area, perimeter, high, volume;
+        length = scan.nextFloat();
+        width = scan.nextFloat();
+        high = scan.nextFloat();
+        area = length * width;
+        perimeter = 2 * length + 2 * width;
+        volume = length * width * high;
+        System.out.println("the area of your classroom is: " + area);
+        System.out.println("the perimeter of your classroom is: " + perimeter);
+        System.out.println("and the volume of your classroom is: " + volume);
+
+
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,40 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        /* Main method (entry point to the application)*/
+
+        System.out.print("Hello,");
+        System.out.print(" World!");
+        int myFavoriteNumber = 23;
+        System.out.println(myFavoriteNumber);
+        String myString = "this is a string variable";
+        System.out.println(myString);
+        myString = "a";
+//        char myString = "this is a string variable";
+//        myString = 3.14159;
+//        long myNumber;
+        float myNumber = 123;
+        System.out.println(myNumber);
+//        long class = 23;
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x +=  5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /=  y;
+        System.out.println(x);
+        y -= x;
+        System.out.println(y);
+
+    }
+}
